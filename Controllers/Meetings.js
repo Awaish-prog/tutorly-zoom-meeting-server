ebf7f34f90863238a2c1a15692fe63388bdb891a
Awaish-prog <awaishk640@gmail.com>
2023-04-07 09:02:03 +0530

    Added meetings list

--- Controllers/Meetings.js
+++ Controllers/Meetings.js
@@ -15,44 +15,99 @@ const acuity = Acuity.basic({
   
 // });
 
-function getTutorMeetings(email, role){
-    let calendarID = null
+
+function getCalendarId(calendars, email, calendarID){
     let tutorEmail = ""
-    let id = ""
-    acuity.request('calendars', function (err, res, calendars) {
-        if (err) return console.error(err);
-        for(let i = 0; i < calendars.length; i++){
-            tutorEmail = calendars[i].email
-            if(tutorEmail.includes(",")){
-                let emails = tutorEmail.split(",")
-                emails.forEach((emailVal) => {
-                    emailVal = emailVal.trim()
-                    if(!emailVal.includes("scheduling") && emailVal !== "admin@tutorly.com" && emailVal !== "admin@mytutorly.com"){
-                        tutorEmail = emailVal
-                    }
-                })
-            }
-            if(tutorEmail.includes("@my")){
-                tutorEmail = tutorEmail.replace("@my", "@")
-            }
-            if(tutorEmail === email){
-                id = calendars[i].id
-            }
+    for(let i = 0; i < calendars.length; i++){
+        tutorEmail = calendars[i].email
+        if(tutorEmail.includes(",")){
+            let emails = tutorEmail.split(",")
+            emails.forEach((emailVal) => {
+                emailVal = emailVal.trim()
+                if(!emailVal.includes("scheduling") && emailVal !== "admin@tutorly.com" &&emailVal !== "admin@mytutorly.com"){
+                    tutorEmail = emailVal
+                }
+            })
         }
-        console.log(id);
-    });
+        if(tutorEmail.includes("@my")){
+            tutorEmail = tutorEmail.replace("@my", "@")
+        }
+        if(tutorEmail === email){
+            calendarID = calendars[i].id
+            return calendarID
+        }
+    }
+    return calendarID
 }
 
+function getMeetingsList(appointments, upcoming) {
+    const tutorAppointments = []
+    appointments.forEach((appointment) => {
+        if(new Date(appointment.datetime) > new Date() && upcoming){
+            tutorAppointments.push(appointment)
+        }
+        else if(new Date(appointment.datetime) < new Date() && !upcoming){
+            tutorAppointments.push(appointment)
+        } 
+    })
+    return tutorAppointments
+}
+
+
 function getPreviousMeetings(req, res){
-    console.log(req.params);
+    const upcoming = false
+    if(req.params.role === 'tutor'){
+        const email = req.params.email.toLowerCase()
+        let calendarID = null
+        acuity.request('calendars', function (err, r1, calendars) {
+            if (err) return console.error(err);
+            calendarID = getCalendarId(calendars, email, calendarID)
+            if(!calendarID){
+                res.json({status: 404, message: "This Email does not exist"})
+                return
+            }
+            acuity.request(`appointments?calendarID=${calendarID}&max=2147483647`, function (err, r2, appointments) {
+                if (err) return console.error(err);
+                const tutorMeetings = getMeetingsList(appointments, upcoming)
+                res.json({status: 200, meetings: tutorMeetings})
+            })
+        })
+    }
+    else{
+        console.log(req.params.email);
+        acuity.request(`appointments?email=${req.params.email.toLowerCase()}&max=2147483647`, function (err, r, appointments) {
+        if (err) return console.error(err);
+        const studentMeetings = getMeetingsList(appointments, upcoming)
+        res.json({status: 200, meetings: studentMeetings})
+        });
+    }
 }
 
 function getUpcomingMeetings(req, res){
+    const upcoming = true
     if(req.params.role === 'tutor'){
-        getTutorMeetings(req.params.email, req.params.role)
+        const email = req.params.email.toLowerCase()
+        let calendarID = null
+        acuity.request('calendars', function (err, r1, calendars) {
+            if (err) return console.error(err);
+            calendarID = getCalendarId(calendars, email, calendarID)
+            if(!calendarID){
+                res.json({status: 404, message: "This Email does not exist"})
+                return
+            }
+            acuity.request(`appointments?calendarID=${calendarID}&max=2147483647`, function (err, r2, appointments) {
+                if (err) return console.error(err);
+                const tutorMeetings = getMeetingsList(appointments, upcoming)
+                res.json({status: 200, meetings: tutorMeetings})
+            })
+        })
     }
     else{
-
+        acuity.request(`appointments?email=${req.params.email.toLowerCase()}&max=2147483647`, function (err, r, appointments) {
+        if (err) return console.error(err);
+        const studentMeetings = getMeetingsList(appointments, upcoming)
+        res.json({status: 200, meetings: studentMeetings})
+        });
     }
 }
 
--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 const express = require('express');
 const { getPreviousMeetings, getUpcomingMeetings } = require('./Controllers/Meetings');
 const app = express()
+const cors = require('cors')
+const request = require('request')
+
+app.use(cors())
 
 app.get("/getPreviousMeetings/:email/:role", getPreviousMeetings)
 
 app.get("/getUpcomingMeetings/:email/:role", getUpcomingMeetings)
+
+var options = {
+  method: 'GET',
+  // A non-existing sample userId is used in the example below.
+  url: 'https://api.zoom.us/v2/users/antonia@tutorly.com/meetings',
+  headers: {
+    authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOm51bGwsImlzcyI6Ilg3a2d4RVg0UmFlWmhtcG55Y1dCRmciLCJleHAiOjE2ODA4NDI1MjksImlhdCI6MTY4MDgzNzEyOX0.oV_WenOWQIufvG8tDE1TzU_bpnTkyxdi_almf9m5ZPo', // Do not publish or share your token publicly.
+  },
+};
+
+function makeR(){
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+    const j = JSON.stringify(JSON.parse(body), null, 2);
+    console.log(j);
+  });
+}
+
+
+makeR()
 
 
 app.listen("4000", () => {
