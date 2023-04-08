410998258392b14bb8f98de77f3ec230640016b4
Awaish-prog <awaishk640@gmail.com>
2023-04-08 09:31:07 +0530

    Added video download with recording event

--- index.js
+++ index.js
@@ -18,70 +18,70 @@ app.get("/getUpcomingMeetings/:email/:role", getUpcomingMeetings)
 
 app.post("/getEvent", (req, res) => {
   
-  const recording = req.body.payload.object.recording_files[0]
-  const downloadUrl = recording.download_url
-  console.log(downloadUrl);
+  // const recording = req.body.payload.object.recording_files[0]
+  // const downloadUrl = recording.download_url
+  // console.log(downloadUrl);
     
-    const dl = new DownloaderHelper(downloadUrl, __dirname);
+  //   const dl = new DownloaderHelper(downloadUrl, __dirname);
 
-    dl.on('end', () => {
-      //printMeetingDetails(recording.meeting_id)
-      console.log("here");
-    });
-    dl.on('error', (err) => console.log('Download Failed', err));
-    dl.start().catch(err => console.error(err));
-  // var response
+  //   dl.on('end', () => {
+  //     //printMeetingDetails(recording.meeting_id)
+  //     console.log("here");
+  //   });
+  //   dl.on('error', (err) => console.log('Download Failed', err));
+  //   dl.start().catch(err => console.error(err));
+  var response
 
-  // console.log(req.headers)
-  // console.log(req.body)
+  console.log(req.headers)
+  console.log(req.body)
 
-  // // construct the message string
-  // const message = `v0:${req.headers['x-zm-request-timestamp']}:${JSON.stringify(req.body)}`
+  // construct the message string
+  const message = `v0:${req.headers['x-zm-request-timestamp']}:${JSON.stringify(req.body)}`
 
-  // const hashForVerify = crypto.createHmac('sha256', "f2E7AulvQuGM18R5mwfZDg").update(message).digest('hex')
+  const hashForVerify = crypto.createHmac('sha256', "f2E7AulvQuGM18R5mwfZDg").update(message).digest('hex')
 
-  // // hash the message string with your Webhook Secret Token and prepend the version semantic
-  // const signature = `v0=${hashForVerify}`
+  // hash the message string with your Webhook Secret Token and prepend the version semantic
+  const signature = `v0=${hashForVerify}`
 
-  // // you validating the request came from Zoom https://marketplace.zoom.us/docs/api-reference/webhook-reference#notification-structure
-  // if (req.headers['x-zm-signature'] === signature) {
+  // you validating the request came from Zoom https://marketplace.zoom.us/docs/api-reference/webhook-reference#notification-structure
+  if (req.headers['x-zm-signature'] === signature) {
 
-  //   // Zoom validating you control the webhook endpoint https://marketplace.zoom.us/docs/api-reference/webhook-reference#validate-webhook-endpoint
-  //   if(req.body.event === 'endpoint.url_validation') {
-  //     const hashForValidate = crypto.createHmac('sha256', "f2E7AulvQuGM18R5mwfZDg").update(req.body.payload.plainToken).digest('hex')
+    // Zoom validating you control the webhook endpoint https://marketplace.zoom.us/docs/api-reference/webhook-reference#validate-webhook-endpoint
+    if(req.body.event === 'endpoint.url_validation') {
+      const hashForValidate = crypto.createHmac('sha256', "f2E7AulvQuGM18R5mwfZDg").update(req.body.payload.plainToken).digest('hex')
 
-  //     response = {
-  //       message: {
-  //         plainToken: req.body.payload.plainToken,
-  //         encryptedToken: hashForValidate
-  //       },
-  //       status: 200
-  //     }
+      response = {
+        message: {
+          plainToken: req.body.payload.plainToken,
+          encryptedToken: hashForValidate
+        },
+        status: 200
+      }
 
-  //     console.log(response.message)
+      console.log(response.message)
 
-  //     res.status(response.status)
-  //     res.json(response.message)
-  //   } else {
-  //     response = { message: 'Authorized request to Zoom Webhook sample.', status: 200 }
+      res.status(response.status)
+      res.json(response.message)
+    } else {
+      response = { message: 'Authorized request to Zoom Webhook sample.', status: 200 }
 
-  //     console.log(response.message)
+      console.log(response.message)
 
-  //     res.status(response.status)
-  //     res.json(response)
+      res.status(response.status)
+      res.json(response)
 
-  //     // business logic here, example make API request to Zoom or 3rd party
+      // business logic here, example make API request to Zoom or 3rd party
 
-  //   }
-  // } else {
+    }
+  } else {
 
-  //   response = { message: 'Unauthorized request to Zoom Webhook sample.', status: 401 }
+    response = { message: 'Unauthorized request to Zoom Webhook sample.', status: 401 }
 
-  //   console.log(response.message)
+    console.log(response.message)
 
-  //   res.status(response.status)
-  //   res.json(response)
-  // }
+    res.status(response.status)
+    res.json(response)
+  }
 })
 
 
@@ -108,6 +108,6 @@ function makeR(){
 //makeR()
 
 
-app.listen("4000", () => {
+app.listen("4005", () => {
   console.log("server running");
 })
