866c8cb14254da48f20c1aac0b9910849544d775
Awaish-prog <awaishk640@gmail.com>
2023-04-08 06:57:10 +0530

    Added listen event

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ const { DownloaderHelper } = require('node-downloader-helper');
 
 
 app.use(cors())
-app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get("/getPreviousMeetings/:email/:role", getPreviousMeetings)
 
