c2b57b7932a181cb89a25cfca24e4898f47c0a94
Awaish-prog <awaishk640@gmail.com>
2023-04-08 08:00:16 +0530

    Added listen event

--- index.js
+++ index.js
@@ -18,16 +18,57 @@ app.get("/getUpcomingMeetings/:email/:role", getUpcomingMeetings)
 
 app.post("/getEvent", (req, res) => {
   
-  if(req.body.event === 'endpoint.url_validation') {
-    const hashForValidate = crypto.createHmac('sha256', "f2E7AulvQuGM18R5mwfZDg").update(req.body.payload.plainToken).digest('hex')
-    console.log(req.body);
-          
-    
-    res.status(200)
-    response.json({
-      "plainToken": request.body.payload.plainToken,
-      "encryptedToken": hashForValidate
-    })
+  var response
+
+  console.log(req.headers)
+  console.log(req.body)
+
+  // construct the message string
+  const message = `v0:${req.headers['x-zm-request-timestamp']}:${JSON.stringify(req.body)}`
+
+  const hashForVerify = crypto.createHmac('sha256', "f2E7AulvQuGM18R5mwfZDg").update(message).digest('hex')
+
+  // hash the message string with your Webhook Secret Token and prepend the version semantic
+  const signature = `v0=${hashForVerify}`
+
+  // you validating the request came from Zoom https://marketplace.zoom.us/docs/api-reference/webhook-reference#notification-structure
+  if (req.headers['x-zm-signature'] === signature) {
+
+    // Zoom validating you control the webhook endpoint https://marketplace.zoom.us/docs/api-reference/webhook-reference#validate-webhook-endpoint
+    if(req.body.event === 'endpoint.url_validation') {
+      const hashForValidate = crypto.createHmac('sha256', "f2E7AulvQuGM18R5mwfZDg").update(req.body.payload.plainToken).digest('hex')
+
+      response = {
+        message: {
+          plainToken: req.body.payload.plainToken,
+          encryptedToken: hashForValidate
+        },
+        status: 200
+      }
+
+      console.log(response.message)
+
+      res.status(response.status)
+      res.json(response.message)
+    } else {
+      response = { message: 'Authorized request to Zoom Webhook sample.', status: 200 }
+
+      console.log(response.message)
+
+      res.status(response.status)
+      res.json(response)
+
+      // business logic here, example make API request to Zoom or 3rd party
+
+    }
+  } else {
+
+    response = { message: 'Unauthorized request to Zoom Webhook sample.', status: 401 }
+
+    console.log(response.message)
+
+    res.status(response.status)
+    res.json(response)
   }
 })
 
