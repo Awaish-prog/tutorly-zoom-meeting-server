925f7e73962e993dce157e807c68cb6be18741fb
Awaish-prog <awaishk640@gmail.com>
2023-04-11 10:45:44 +0530

    added google drive file upload

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const request = require('request')
 const bodyParser = require('body-parser');
 const { DownloaderHelper } = require('node-downloader-helper');
 const crypto = require('crypto')
+const path = require('path');
 
 const fs = require('fs')
 const {google} = require('googleapis')
@@ -13,10 +14,10 @@ const {google} = require('googleapis')
 const KEYFILEPATH = __dirname + "/google-key.json"
 const SCOPES = ['https://www.googleapis.com/auth/drive']
 
-const auth = new google.auth.GoogleAuth({
-  keyFile: KEYFILEPATH,
-  scopes: SCOPES
-})
+// const auth = new google.auth.GoogleAuth({
+//   keyFile: KEYFILEPATH,
+//   scopes: SCOPES
+// })
 
 
 app.use(cors())
@@ -46,7 +47,45 @@ async function uploadFile(auth) {
   console.log(res);
 }
 
-uploadFile(auth)
+
+
+
+
+function upload2(){
+  const auth = new google.auth.JWT(
+    "tutorlyrecording@tutorly-recordings.iam.gserviceaccount.com",
+    null,
+    "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCcI+rD2WFQtkpN\nLZHzaHyey0EWcZgUdkj52clQZsTziokcR9p25+rR/RIdf2+1Kx8lJKUsWkQdN3bB\nTvbbDi7YaqojPuvQYjmFB0g1jJM11zQBRH9z9iALWutx0CCmYUSqwipkjaxpX5hR\n0f4pkhc8/tWsZu6UY4EhLW163E3v+Quq7L+vv20SDOsSs/Eg8DWAeOXmSp1vhjAr\n5WhLkzhsPuYVEk1mm0yEliYJuAmnxfnAXPksqZuRMyHb0a5zcl1Gk0c+1tb7CRiY\n9rrA26K13QhXhOR4IkymIevd1uVBTDm8ccCtave8cuDNqvkdCnRkHYf21s154mMV\nozVna0f1AgMBAAECggEACXdJ+SP5eOBcTSUunIBLUdXbtSXKL69jdDZGX1KjrRTL\nLto8/d7IhyuIXh5xjMe3pH5TadwdFgex8j08d6z6nRHhDQWjBpKgjxaRGPdT82cq\nbCCVeNfFOzsAsXk0iYcLF9kb+IBNoWPIwxPFN2BNJVxZDZHTSU67r4xmqen86vKT\ncFOhzgY8pvgqQSBBwIIO2B3o70L/E37axAhTnNQS6XuneFYaEKt4ZaGmRTIaBeAo\nstodh1wGyAd5werrK33b/NXd8rgCgxHFA5wWfLhy06i6GpC51SxSV5fiI9MDpEzC\nDR6QDKx+ZAD/gcIY5P+FRKmQH/0e/QCG8mB2SS6+0QKBgQDbY663F9uj+mhjgzB8\n96fpjKgosSfrBST6wheRQ1LGAQUZ+RoYTOCtKI6SihHmvhwNIr5RpOhPvw38/qSw\nckJpYP0jSQCcl9Tvs+W8gWjLN46xSQjBXv704FgcWmQ20rp6jypIqCPropfp1Sqe\nkRiBTG05K9qX/nWaWKmN2xvsXQKBgQC2Mjub4BVa0GR1b7coYfO6aQMRblAW0q2x\nwNiXPr0Y2rizGXO6+SJ+YpaDwUKNki9vDtxS4PJ7Kgvbe4cxEq3Ey4hzqw4hMHz5\nOQPXvY6vJuD1zHwtBvrNoqiBbgDq7111eWuEvdHEBeC1kPO7Mxk5sA9x57eD0CQ/\nWnbV+RHQeQKBgFilmaYvxLcoal+zVbdZFob/J79xfdCv/nY4UO+IRXzuUpJhHhMt\nlSdCmQFLOtqCfLEuFbrFqHz4lP1iI0eMk5si7oYAbHfZvdexWCXoIGDkrQqjmR8R\ndHGb6N5kA9RHJ8R37rz2+7StuTZpxNPXixBLwmJ0ftolnn1kT/aaz1idAoGBAKce\nuJKoHq5oTF10FZsTDg9E162bdBcbNHvBmmpQ//7WUWjmxcMRVtR69fjwwm09VAkY\npnvXHlRfIvU1/7HBombLEuSWWKWU5m1sINw5YbHDhcGwyY47wr3wn4PpNSpKWX2x\n3C+sNccKZS3mQcrKei7yEZi3pRthRn5eKnbmubJxAoGAftKJGMIVl3jSNnx/99Lc\nDN9drj2iNhIi8YemfhDjGqsHYblYm54msBcGc4rYKZ7z3kGbILYhq7HROhBSkdFQ\nb5D5IV3PWv87qv1lBJjiyZ46YjnFcwrl9oWFiBbY79YCsj5QgJBa84tCfSmNsq5p\nsMF5m00mLPrvPyPvpH878+w=\n-----END PRIVATE KEY-----\n",
+    ['https://www.googleapis.com/auth/drive']
+  );
+  
+  const drive = google.drive({ version: 'v3', auth });
+
+  const file = fs.createReadStream(path.join(__dirname, 'GMT20230411-030257_Recording_640x360.mp4'));
+
+  drive.files.create({
+    resource: {
+      name: 'GMT20230411-030257_Recording_640x360.mp4',
+      parent: ['meetingtest']
+    },
+    media: {
+      mimeType: 'video/mp4',
+      body: file,
+    },
+  }, (err, res) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(`File uploaded: ${res.data.id}`);
+    }
+  });
+  
+}
+
+
+
+
+upload2()
 app.post("/getEvent", (req, res) => {
   
 
