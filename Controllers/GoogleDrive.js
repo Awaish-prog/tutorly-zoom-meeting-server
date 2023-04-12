e02ee79388ffb77a96977afb204def498eed41d2
Awaish-prog <awaishk640@gmail.com>
2023-04-12 07:48:15 +0530

    added google drive file upload and web link

--- Controllers/GoogleDrive.js
+++ Controllers/GoogleDrive.js
@@ -44,7 +44,7 @@ async function getWebLink(id, fileName){
       fileId: id,
       fields: 'webViewLink'
     })
-    console.log(`web link: ${JSON.parse(JSON.stringify(x))}`);
+    console.log(`web link: ${x.config.data}`);
     fs.unlinkSync(path.join(__dirname, fileName))
 }
   
