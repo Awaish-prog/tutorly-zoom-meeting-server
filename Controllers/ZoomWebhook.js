0004ac30b20df9af47e316fbc1b737bfa6495e65
Awaish-prog <awaishk640@gmail.com>
2023-04-13 08:57:59 +0530

    Added recording link to frontend

--- Controllers/ZoomWebhook.js
+++ Controllers/ZoomWebhook.js
@@ -43,7 +43,8 @@ function downloadRecording(req, res){
       req.body.payload.object.recording_files[0] :
       req.body.payload.object.recording_files[1]
     const downloadUrl = recording.download_url
-    let f = String(req.body.payload.object.topic) + " " + String(new Date(req.body.payload.object.start_time).toLocaleString('en-US', { timeZone: 'America/Los_Angeles' }))
+    let f = String(req.body.payload.object.topic) + "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"
+    //String(new Date(req.body.payload.object.start_time).toLocaleString('en-US', { timeZone: 'America/Los_Angeles' })))
     const dl = new DownloaderHelper(downloadUrl, __dirname, {fileName: f});
 
     dl.on('end', () => {
