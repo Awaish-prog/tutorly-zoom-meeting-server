3f5f66716e5703bb89c3b5f7956267a71714a85b
Awaish-prog <awaishk640@gmail.com>
2023-04-20 07:37:10 +0530

    Added google drive folder upload

--- Controllers/GoogleDrive.js
+++ Controllers/GoogleDrive.js
@@ -76,7 +76,8 @@ async function uploadFileAndGetWebLink(fileName, host_email, start_time){
                         }
                     };
                     const id = appointment.id
-                    acuity.request(`appointments/${id}?admin=true`, options, function (err, res, appointment) {
+                    console.log(`appointments/${id}?admin=true`);
+                    acuity.request('appointments/'+ id +'?admin=true', options, function (err, res, appointment) {
                         if (err) return console.error(err);
                         console.log(appointment);
                     });    
