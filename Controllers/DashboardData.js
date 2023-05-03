6585a5cc7d82d25b3299fe65ff3bd2f24ae5d9de
Awaish-prog <awaishk640@gmail.com>
2023-05-03 07:10:05 +0530

    changed date for meeting sheets data

--- Controllers/DashboardData.js
+++ Controllers/DashboardData.js
@@ -84,6 +84,8 @@ async function googleSheetTest(req, res){
       }
       else{
         if(item.canceled){
+          console.log((new Date(appointments[i].datetime)).toLocaleString('en-US', { timeZone: 'America/Los_Angeles' }).replaceAll("/", "-"));
+          console.log((new Date(appointments[i].endDatetime)).toLocaleString('en-US', { timeZone: 'America/Los_Angeles' }).replaceAll("/", "-"));
         values.push([(new Date(appointments[i].datetime)).toLocaleString('en-US', { timeZone: 'America/Los_Angeles' }).replaceAll("/", "-"), (new Date(appointments[i].endDatetime)).toLocaleString('en-US', { timeZone: 'America/Los_Angeles' }).replaceAll("/", "-"), appointments[i].firstName, appointments[i].lastName, appointments[i].phone, appointments[i].email, appointments[i].type, appointments[i].calendar, appointments[i].priceSold, appointments[i].paid, appointments[i].price, appointments[i].certificate, appointments[i].notes, appointments[i].datetimeCreated, appointments[i].labels[0].name, "Yes", appointments[i].id]);
       }
       else{
