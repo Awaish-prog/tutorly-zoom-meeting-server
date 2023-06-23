b0290a16c9c7fdb3f466538cc64d637e606573d4
Personal Use <awaishk640@gmail.com>
2023-06-24 04:23:06 +0530

    Added new socket

--- socket_app.js
+++ socket_app.js
@@ -2,18 +2,25 @@ const io = require("socket.io")(8080, {
     cors: {
         origin: "*"
     }
-});
-
-io.on("connection", (socket) => {
-    socket.on("syncBoard", (dataURL) => {
+  });
+  
+  io.on("connection", (socket) => {
+    socket.on("syncBoard", (dataURL, currentPageSource) => {
         console.log("Board data received on server..");
-        socket.to("board").emit("received", { dataURL });
+        socket.to("board").emit("received", { dataURL, currentPageSource });
     })
-    socket.on("syncErasedData", (eraserData) => {
-      socket.to("board").emit("eraseData", { eraserData });
-  })
+    socket.on("syncErasedData", (eraserData, currentPageSource, dataURL) => {
+      socket.to("board").emit("eraseData", { eraserData, currentPageSource, dataURL });
+    })
+    socket.on("addText", (currentPageSource, dataURL) => {
+      socket.to("board").emit("addText", { currentPageSource, dataURL });
+    })
+    socket.on("addPage", () => {
+      socket.to("board").emit("addPage");
+    })
     socket.on("joinWhiteBoard", (board) => {
         socket.join(board);
         console.log("Joined board");
     })  
   })
+  
