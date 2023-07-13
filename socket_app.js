82176d67bd338c09ee65230c4d580755e8a1230b
Personal Use <awaishk640@gmail.com>
2023-07-13 11:42:59 +0530

    Added refresh

--- socket_app.js
+++ socket_app.js
@@ -1,35 +1,35 @@
-const io = require("socket.io")(8080, {
-    cors: {
-        origin: "*"
-    }
-  });
+// const io = require("socket.io")(8080, {
+//     cors: {
+//         origin: "*"
+//     }
+//   });
   
-  io.on("connection", (socket) => {
-    socket.on("syncBoard", (dataURL, currentPageSource) => {
-        console.log("Board data received on server..");
-        socket.to("board").emit("received", { dataURL, currentPageSource });
-    })
-    socket.on("syncErasedData", (eraserData, currentPageSource, dataURL) => {
-      socket.to("board").emit("eraseData", { eraserData, currentPageSource, dataURL });
-    })
-    socket.on("addText", (currentPageSource, dataURL) => {
-      socket.to("board").emit("addText", { currentPageSource, dataURL });
-    })
-    socket.on("addPage", () => {
-      socket.to("board").emit("addPage");
-    })
-    socket.on("joinWhiteBoard", (board) => {
-        socket.join(board);
-        console.log("Joined board");
-    })  
-    socket.on("undo", (dataURL, currentPageSource, x, y, index, obj) => {
-      console.log(currentPageSource, x, y, index);
-      socket.to("board").emit("undo", { dataURL, currentPageSource, x, y, index, obj });
-    })
-    socket.on("redo", (undoObj, index) => {
-      socket.to("board").emit("redo", { undoObj, index });
-    })
-    socket.on("syncImage", (imageData, currentPageSource, imageX, imageY, imageWidth, imageHeight, dataURL) => {
-      socket.to("board").emit("syncImage", { imageData, currentPageSource, imageX, imageY, imageWidth, imageHeight, dataURL });
-    })
-  })
+//   io.on("connection", (socket) => {
+//     socket.on("syncBoard", (dataURL, currentPageSource) => {
+//         console.log("Board data received on server..");
+//         socket.to("board").emit("received", { dataURL, currentPageSource });
+//     })
+//     socket.on("syncErasedData", (eraserData, currentPageSource, dataURL) => {
+//       socket.to("board").emit("eraseData", { eraserData, currentPageSource, dataURL });
+//     })
+//     socket.on("addText", (currentPageSource, dataURL) => {
+//       socket.to("board").emit("addText", { currentPageSource, dataURL });
+//     })
+//     socket.on("addPage", () => {
+//       socket.to("board").emit("addPage");
+//     })
+//     socket.on("joinWhiteBoard", (board) => {
+//         socket.join(board);
+//         console.log("Joined board");
+//     })  
+//     socket.on("undo", (dataURL, currentPageSource, x, y, index, obj) => {
+//       console.log(currentPageSource, x, y, index);
+//       socket.to("board").emit("undo", { dataURL, currentPageSource, x, y, index, obj });
+//     })
+//     socket.on("redo", (undoObj, index) => {
+//       socket.to("board").emit("redo", { undoObj, index });
+//     })
+//     socket.on("syncImage", (imageData, currentPageSource, imageX, imageY, imageWidth, imageHeight, dataURL) => {
+//       socket.to("board").emit("syncImage", { imageData, currentPageSource, imageX, imageY, imageWidth, imageHeight, dataURL });
+//     })
+//   })
