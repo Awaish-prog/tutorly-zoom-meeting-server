d13f57c8b4e164f385a23b735465273d01e486d4
Personal Use <awaishk640@gmail.com>
2023-08-16 13:07:46 +0530

    Added slack

--- index.js
+++ index.js
@@ -29,9 +29,9 @@ app.use(express.static(path.join(__dirname, 'white-board')))
 
 
 app.post("/slackMessage", (req, res) => {
-  //req.body.event.userName = getUserName(req.body.event.user)
+  req.body.event.userName = getUserName(req.body.event.user)
   updateUsersAndReads(req.body);
-  //outer_socket.emit("sendMessage", req.body.event)
+  outer_socket.emit("sendMessage", req.body.event)
 })
 
 app.get("/getPreviousMeetings/:email/:role/:number", authentication, getPreviousMeetings)
@@ -101,22 +101,22 @@ app.get("*", (req, res) => {
 })
 
 
-const io = require("socket.io")(8080, {
-  cors: {
-      origin: "*"
-  }
-});
+// const io = require("socket.io")(8080, {
+//   cors: {
+//       origin: "*"
+//   }
+// });
 
 
 
-io.on("connection", (socket) => {
-  outer_socket = socket
-  console.log("connected");
+// io.on("connection", (socket) => {
+//   outer_socket = socket
+//   console.log("connected");
 
-  socket.on("postMessage", (channel, userName, text, showThread, ts) => {
-     postMessage(channel, userName, text, showThread, ts)
-  })
-})
+//   socket.on("postMessage", (channel, userName, text, showThread, ts) => {
+//      postMessage(channel, userName, text, showThread, ts)
+//   })
+// })
 
 
 
