c19ea423de04e69aa4f59b3a85176c0922b88326
Personal Use <awaishk640@gmail.com>
2023-08-22 03:29:56 +0530

    added channel timestamp update

--- Controllers/Slack.js
+++ Controllers/Slack.js
@@ -344,6 +344,12 @@ async function getChat(req, res){
 
     if(userId && usersAndReads[userId] && usersAndReads[userId][channel]){
         usersAndReads[userId][channel].lastRead = usersAndReads[userId][channel].latestMessage
+        if(slackTokens[slackMembers[email]]){
+            const client = new WebClient(slackTokens[slackMembers[email]])
+            const res = client.conversations.mark({channel: usersAndReads[userId][channel], ts: usersAndReads[userId][channel].latestMessage})
+            console.log(res);
+        }
+        
         console.log("Marked read");
     }
 }
