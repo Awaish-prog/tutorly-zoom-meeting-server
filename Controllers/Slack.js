b747c64e28d9d0be5b1a2276365a059abee0be82
Personal Use <awaishk640@gmail.com>
2023-08-23 06:56:38 +0530

    fixed notification bug

--- Controllers/Slack.js
+++ Controllers/Slack.js
@@ -336,7 +336,7 @@ async function getChat(req, res){
             text: messages[i].text,
             ts: messages[i].ts,
             replyCount: messages[i].reply_count ? messages[i].reply_count : 0,
-            read: i > 1 ? true : false /*con.channel.is_member && usersAndReads[userId] && usersAndReads[userId][channel] && usersAndReads[userId][channel].lastRead && usersAndReads[userId][channel].lastRead < messages[i].ts*/
+            read: i < 7 ? true : false /*con.channel.is_member && usersAndReads[userId] && usersAndReads[userId][channel] && usersAndReads[userId][channel].lastRead && usersAndReads[userId][channel].lastRead < messages[i].ts*/
         })
     }
 
