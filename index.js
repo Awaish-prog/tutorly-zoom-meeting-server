88b3fef44449178813fa49497290bcc59cd2e57a
Personal Use <awaishk640@gmail.com>
2023-08-24 07:29:03 +0530

    fixed notification bug

--- index.js
+++ index.js
@@ -29,26 +29,26 @@ app.use(express.static(path.join(__dirname, 'white-board')))
 
 
 app.post("/slackMessage", (req, res) => {
-  // req.body.event.userName = getUserName(req.body.event.user)
+  req.body.event.userName = getUserName(req.body.event.user)
   
-  // const event = {
-  //   userName: req.body.event.userName,
-  //   ts: req.body.event.ts,
-  //   channel: req.body.event.channel,
-  //   event_ts: req.body.event.event_ts,
-  //   text: req.body.event.text,
-  //   user: req.body.event.user,
-  //   type: req.body.event.type,
-  //   thread_ts: req.body.event.thread_ts
-  // }
+  const event = {
+    userName: req.body.event.userName,
+    ts: req.body.event.ts,
+    channel: req.body.event.channel,
+    event_ts: req.body.event.event_ts,
+    text: req.body.event.text,
+    user: req.body.event.user,
+    type: req.body.event.type,
+    thread_ts: req.body.event.thread_ts
+  }
 
 
 
-  // outer_socket.emit("sendNotification")
-  // outer_socket.emit("sendMessage", event)
-  // console.log(event);
+  outer_socket.emit("sendNotification")
+  outer_socket.emit("sendMessage", event)
+  console.log(outer_socket.emit);
   
-  // updateUsersAndReads(req.body);
+  updateUsersAndReads(req.body);
 })
 
 app.get("/getPreviousMeetings/:email/:role/:number", authentication, getPreviousMeetings)
@@ -120,26 +120,26 @@ app.get("*", (req, res) => {
 })
 
 
-// const io = require("socket.io")(8080, {
-//   cors: {
-//       origin: "*"
-//   }
-// });
+const io = require("socket.io")(8080, {
+  cors: {
+      origin: "*"
+  }
+});
 
 
 
-// io.on("connection", (socket) => {
-//   outer_socket = socket
-//   console.log("connected");
+io.on("connection", (socket) => {
+  outer_socket = socket
+  console.log("connected");
 
-//   socket.on("postMessage", (channel, userName, text, showThread, ts) => {
-//      postMessage(channel, userName, text, showThread, ts)
-//   })
+  socket.on("postMessage", (channel, userName, text, showThread, ts) => {
+     postMessage(channel, userName, text, showThread, ts)
+  })
 
-//   socket.on("markMessageAsRead", (email, channel) => {
-//     markMessageAsReadSocket(email, channel);
-//  })
-// })
+  socket.on("markMessageAsRead", (email, channel) => {
+    markMessageAsReadSocket(email, channel);
+ })
+})
 
 
 
@@ -156,7 +156,7 @@ app.listen("4005", async () => {
 
   //createNewSheet()
   //markStatus()
-  //initializeSlackIds()
+  initializeSlackIds()
   //populateConversationStore()
 
   console.log("server running");
