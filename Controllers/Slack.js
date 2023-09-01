95d3dfe26f6e2613b609d75fba4b4f5f29678d68
Personal Use <awaishk640@gmail.com>
2023-09-02 03:42:57 +0530

    Added slack ids data

--- Controllers/Slack.js
+++ Controllers/Slack.js
@@ -431,7 +431,7 @@ async function getSlackFileUrl(req, res){
 }
 
 async function postMessage(channel, userName, text, showThread, ts){
-    const client = new WebClient(slackTokens[userName] ? slackTokens[userName] : slackTokens["U02S69ZB9NG"])
+    // const client = new WebClient(slackTokens[userName] ? slackTokens[userName] : slackTokens["U02S69ZB9NG"])
     if(showThread){
         const result = await client.chat.postMessage({
             channel: channel,
